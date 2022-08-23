b0cdc05c3e36bdea86b6a47bd4ddffc2e0b3d7ef
Felipe Rosas <felipe.rosas@g.globo>
2022-08-23 20:39:09 -0300

    chore: adiciona o sql de migracao inicial

---
+++ database/migrations/2022-08-23-20-35-00_inicial/migration.sql
@@ -0,0 +1,28 @@
+
+CREATE TABLE IF NOT EXISTS states (
+  id serial PRIMARY KEY,
+  name varchar(2) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS cities (
+  id serial PRIMARY KEY,
+  name varchar(100) NOT NULL,
+  state_id integer NOT NULL,
+  CONSTRAINT FK_cities_states  FOREIGN KEY (state_id) REFERENCES states (id)
+);
+
+CREATE TABLE IF NOT EXISTS neighborhoods (
+  id serial PRIMARY KEY,
+  name varchar(100) NOT NULL,
+  city_id integer NOT NULL,
+  CONSTRAINT FK_neighborhoods_cities FOREIGN KEY (city_id) REFERENCES cities (id)
+);
+
+CREATE TABLE IF NOT EXISTS streets (
+    id serial PRIMARY KEY,
+    name varchar(150) NOT NULL,
+    neighborhood_id integer NOT NULL,
+    CONSTRAINT FK_streets_neighborhoods FOREIGN KEY (neighborhood_id) REFERENCES neighborhoods (id)
+);
+
+ 
