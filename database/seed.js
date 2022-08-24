83ffe10606ee7fd235510313b6257d89d93e7a31
Felipe Rosas <felipe.rosas@g.globo>
2022-08-23 21:45:35 -0300

    chore: inclui funcionalidade de criar inserts sql para neighborhoods

--- database/seed.js
+++ database/seed.js
@@ -4,6 +4,11 @@ const path = require("path");
 const filePathData = path.join(__dirname, "data.txt");
 const fileSeedStatesPath = path.join(__dirname, "seeds", "states.sql");
 const fileSeedCitiesPath = path.join(__dirname, "seeds", "cities.sql");
+const fileSeedNeighborhoodsPath = path.join(
+  __dirname,
+  "seeds",
+  "neighborhoods.sql"
+);
 
 const data = fs.readFileSync(filePathData, "utf8");
 
@@ -15,14 +20,25 @@ let stateLastId = 1;
 const cities = [];
 let cityLastId = 1;
 
+const neighborhoods = [];
+let neighborhoodLastId = 1;
+
 function generateInsertStateSql(id, name) {
   const sql = `INSERT INTO states (id, name) VALUES (${id}, '${name}');`;
+
   fs.appendFileSync(fileSeedStatesPath, sql + "\n");
 }
 
 function generateInsertCitySql(id, name, stateId) {
   const sql = `INSERT INTO cities (id, name, state_id) VALUES (${id}, '${name}', ${stateId});`;
-  fs.appendFileSync(fileSeedCitiesPath, sql + "\n");
+
+  fs.appendFileSync(fileSeedCitiesPath, sql.trim() + "\n");
+}
+
+function generateInsertNeighborhoodSql(id, name, cityId) {
+  const sql = `INSERT INTO neighborhoods (id, name, city_id) VALUES (${id}, '${name}', ${cityId});`;
+
+  fs.appendFileSync(fileSeedNeighborhoodsPath, sql + "\n");
 }
 
 for (let i = 0; i < dataArray.length; i++) {
@@ -33,6 +49,7 @@ for (let i = 0; i < dataArray.length; i++) {
 
   let currentStateId;
   let currentCityId;
+  let currentNeighborhoodId;
 
   const stateFound = states.find((item) => item.name === state);
   if (stateFound) {
@@ -65,5 +82,27 @@ for (let i = 0; i < dataArray.length; i++) {
     generateInsertCitySql(currentCityId, city, currentStateId);
   }
 
-  console.log(city);
+  const neighborhoodFound = neighborhoods.find(
+    (item) => item.name === neighborhood
+  );
+
+  if (neighborhoodFound) {
+    currentNeighborhoodId = neighborhoodFound.id;
+  }
+
+  if (!neighborhoodFound) {
+    currentNeighborhoodId = neighborhoodLastId++;
+    neighborhoods.push({
+      id: currentNeighborhoodId,
+      name: neighborhood,
+    });
+
+    generateInsertNeighborhoodSql(
+      currentNeighborhoodId,
+      neighborhood,
+      currentCityId
+    );
+  }
 }
+
+console.log("Seeds geradas...");
