705974243e462a5dad2fc48681b9d289cd0f2542
Felipe Rosas <felipe.rosas@g.globo>
2022-08-23 21:37:37 -0300

    chore: inclui funcionalidade de criar inserts sql para cities

--- database/seed.js
+++ database/seed.js
@@ -2,19 +2,27 @@ const fs = require("fs");
 const path = require("path");
 
 const filePathData = path.join(__dirname, "data.txt");
-const fileSeedStatePath = path.join(__dirname, "seeds", "states.sql");
+const fileSeedStatesPath = path.join(__dirname, "seeds", "states.sql");
+const fileSeedCitiesPath = path.join(__dirname, "seeds", "cities.sql");
 
 const data = fs.readFileSync(filePathData, "utf8");
 
 const dataArray = data.split("\n");
 
 const states = [];
-let stateCount = 0;
 let stateLastId = 1;
+
+const cities = [];
+let cityLastId = 1;
 
 function generateInsertStateSql(id, name) {
   const sql = `INSERT INTO states (id, name) VALUES (${id}, '${name}');`;
-  fs.appendFileSync(fileSeedStatePath, sql + "\n");
+  fs.appendFileSync(fileSeedStatesPath, sql + "\n");
+}
+
+function generateInsertCitySql(id, name, stateId) {
+  const sql = `INSERT INTO cities (id, name, state_id) VALUES (${id}, '${name}', ${stateId});`;
+  fs.appendFileSync(fileSeedCitiesPath, sql + "\n");
 }
 
 for (let i = 0; i < dataArray.length; i++) {
@@ -23,7 +31,8 @@ for (let i = 0; i < dataArray.length; i++) {
   const [cep, cityAndState, neighborhood, street] = value.split("\t");
   const [city, state] = cityAndState.split("/");
 
-  let currentStateId = ++stateCount;
+  let currentStateId;
+  let currentCityId;
 
   const stateFound = states.find((item) => item.name === state);
   if (stateFound) {
@@ -38,6 +47,22 @@ for (let i = 0; i < dataArray.length; i++) {
     });
 
     generateInsertStateSql(currentStateId, state);
+  }
+
+  const cityFound = cities.find((item) => item.name === city);
+
+  if (cityFound) {
+    currentCityId = cityFound.id;
+  }
+
+  if (!cityFound) {
+    currentCityId = cityLastId++;
+    cities.push({
+      id: currentCityId,
+      name: city,
+    });
+
+    generateInsertCitySql(currentCityId, city, currentStateId);
   }
 
   console.log(city);
