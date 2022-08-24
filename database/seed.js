1e52fb7bd005051d8e2fac9a984ce6813ec0a997
Felipe Rosas <felipe.rosas@g.globo>
2022-08-23 21:24:27 -0300

    chore: inclui funcionalidade de criar inserts sql para states

--- database/seed.js
+++ database/seed.js
@@ -2,10 +2,20 @@ const fs = require("fs");
 const path = require("path");
 
 const filePathData = path.join(__dirname, "data.txt");
+const fileSeedStatePath = path.join(__dirname, "seeds", "states.sql");
 
 const data = fs.readFileSync(filePathData, "utf8");
 
 const dataArray = data.split("\n");
+
+const states = [];
+let stateCount = 0;
+let stateLastId = 1;
+
+function generateInsertStateSql(id, name) {
+  const sql = `INSERT INTO states (id, name) VALUES (${id}, '${name}');`;
+  fs.appendFileSync(fileSeedStatePath, sql + "\n");
+}
 
 for (let i = 0; i < dataArray.length; i++) {
   const value = dataArray[i];
@@ -13,5 +23,22 @@ for (let i = 0; i < dataArray.length; i++) {
   const [cep, cityAndState, neighborhood, street] = value.split("\t");
   const [city, state] = cityAndState.split("/");
 
-  console.log(city, state, neighborhood, street);
+  let currentStateId = ++stateCount;
+
+  const stateFound = states.find((item) => item.name === state);
+  if (stateFound) {
+    currentStateId = stateFound.id;
+  }
+
+  if (!stateFound) {
+    currentStateId = stateLastId++;
+    states.push({
+      id: currentStateId,
+      name: state,
+    });
+
+    generateInsertStateSql(currentStateId, state);
+  }
+
+  console.log(city);
 }
